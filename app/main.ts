224b25810eee36e42190adb3a93517bb3d7adde4
aishwaryr <araj713@gmail.com>
2026-01-07 17:03:03 +0530

    add type cmd

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,23 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const builtins = ["exit", "echo", "type"];
+
 function repl() {
   rl.question("$ ", (answer: string) => {
     if (answer === "exit") return rl.close();
 
     const ansArray = answer.split(" ");
+
+    if (ansArray[0] === "type") {
+      const cmd = ansArray.slice(1).join(" ");
+      if (builtins.includes(cmd)) {
+        console.log(cmd + " is a shell builtin");
+      } else {
+        console.log("invalid_command: not found");
+      }
+    }
+
     if (ansArray[0] === "echo") {
       console.log(ansArray.slice(1).join(" "));
     } else {
